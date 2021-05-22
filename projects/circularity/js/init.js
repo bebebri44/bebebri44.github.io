669c7ba5d1d8c6d39ee66180aff396195e61fed6
bebebri44 <brianna4dorgilles@gmail.com>
2021-05-22 12:20:40 +0000

    adding circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,21 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
+        for(var loopCompleted = 0;loopCompleted < 100; loopCompleted++) {
+            drawCircle(); 
+        }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,14 +46,15 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            
             
             // TODO 5 : Call game.checkCirclePosition() on your circles.
-           
-
+         
             // TODO 8 : Iterate over the array
-           
-            
+           for (var i = 0; i < circles.length; i++) {
+            var eachValue = circles[i];
+            physikz.updatePosition(eachCircle);
+            game.checkCirclePosition(eachCircle);
         }
     
         /* 
@@ -60,10 +68,13 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
+
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
+            var rightEdge = circle.x + circle.radius;
+            if (circle.x === canvas.width) {
+                circle.x = 0;
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
@@ -89,4 +100,5 @@ if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
+}
 }
